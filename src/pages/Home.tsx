052420c4e6a0f13b0d120947499463b720a04b47
D_water <chrisjerry070@gmail.com>
2023-12-13 20:48:49 +0400

    done with home page display design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import { BgContext, MorePageContext, UserInfo } from "../MyContext";
 import sendSvg from "../assests/money-cash-svgrepo-com.svg";
 import billSvg from "../assests/money-cash-svgrepo-com (1).svg";
-import loadSvg from "../assests/history-svgrepo-com.svg"
-import mobileSvg from "../assests/mobile2-svgrepo-com.svg"
+import loadSvg from "../assests/history-svgrepo-com.svg";
+import mobileSvg from "../assests/mobile2-svgrepo-com.svg";
 import loanImage from "../assests/loan.jpeg";
-import eNaeriaImage from "https://enaira.gov.ng/wp-content/uploads/2023/04/enaira-logo.jpg"
+import eNaeriaImage from "https://enaira.gov.ng/wp-content/uploads/2023/04/enaira-logo.jpg";
 
 export const Home = () => {
   // states
@@ -13,28 +13,27 @@ export const Home = () => {
   const [reload, setReload] = useState<boolean>(true);
   const [changeType, setChangeType] = useState<string>("");
   const [clickTime, setClickTime] = useState<string>("");
+  const [showLoan, setShowLoan] = useState<boolean>(true);
+  const [showEnaira, setShowEnaira] = useState<boolean>(true);
+  const [showService, setShowService] = useState<boolean>(true);
+  const [showMobile, setShowMobile] = useState<boolean>(true);
+  const [showAccount, setShowAccount] = useState<boolean>(true);
+
 
   // contexts
   const { bg } = useContext(BgContext);
   const { setShowNoti } = useContext(MorePageContext);
   const user = useContext(UserInfo);
   useEffect(() => {
-    setTime()
-    
-  }, [])
-  
- 
-
-  
-  
+    setTime();
+  }, []);
 
   // functions
 
   function setTime(): void {
-
     setReload(false);
 
-    setTimeout(()=> {
+    setTimeout(() => {
       const time = new Date().toLocaleTimeString([], {
         hour: "numeric",
         minute: "2-digit",
@@ -43,9 +42,8 @@ export const Home = () => {
 
       setClickTime(time);
       setReload(true);
-    }, 2000 )
+    }, 2000);
   }
-
 
   function handleNotiPage(): void {
     if (setShowNoti !== undefined) {
@@ -53,9 +51,8 @@ export const Home = () => {
     }
   }
 
-
   function handleClick(): void {
-    setTime()
+    setTime();
   }
   return (
     <div
@@ -149,9 +146,11 @@ export const Home = () => {
             </div>
           )}
         </section>
-        <div className="bg-[#f1f1f1] w-full text-black h-[1000px] pt-20 overflow-hidden">
+
+        <div className="bg-[#f1f1f1] w-full text-black h-[920px] pt-20 overflow-hidden">
           <div className="bg-red-600 w-2.5 h-2.5 mx-auto rounded-full -mt-4 "></div>
-          <section className="sec-height mx-auto  w-48 bg-white rounded-lg drop-shadow-xl mt-2  pl-1.5 pt-2 ">
+
+          {showService && <section className="sec-height mx-auto  w-48 bg-white rounded-lg drop-shadow-xl mt-2  pl-1.5 pt-2 ">
             <h2 className="text-sm font-[600]">Service</h2>
             <ul className="w-full h-full flex items-center justify-center gap-1.5 -mt-3">
               <li className="flex flex-col items-center gap-1 cursor-pointer">
@@ -181,42 +180,88 @@ export const Home = () => {
                 </div>
                 <p className="text-[8px]">Mobile</p>
               </li>
-
             </ul>
-          </section>
-          <section className="sec-height mx-auto  w-48 bg-white rounded-lg drop-shadow-xl mt-4 pl-1.5 pt-2">
-            <h2 className="text-sm font-[600]">Loan</h2>
+          </section>}
+
+          {showLoan && <section className="sec-height mx-auto  w-48 bg-white rounded-lg drop-shadow-xl mt-4 pl-1.5 pt-2">
+            <h2 className="text-sm font-[600]">Loans</h2>
             <div className="w-24  mt-0.5 ml-1 cursor-pointer">
-              <img src={loanImage} alt="loan image" className="rounded-md h-[50px]" />
+              <img
+                src={loanImage}
+                alt="loan image"
+                className="rounded-md h-[50px]"
+              />
               <p className="text-[11px] mt-[2px] ml-0.5">Quick Loans</p>
             </div>
-          </section>
-          <section className=" relative mx-auto pb-4 w-48 bg-white rounded-lg drop-shadow-xl mt-4  pt-2">
-            <img src='https://enaira.gov.ng/wp-content/uploads/2023/04/enaira-logo.jpg' alt="eNaria image"  className="h-10 w-24 ml-1.5"/>  
+          </section>}
+          {showEnaira && <section className=" relative mx-auto pb-4 w-48 bg-white rounded-lg drop-shadow-xl mt-4  pt-2">
+            <img
+              src="https://enaira.gov.ng/wp-content/uploads/2023/04/enaira-logo.jpg"
+              alt="eNaria image"
+              className="h-10 w-24 ml-1.5"
+            />
             <div className=" absolute top-0 left-[49px] w-24 h-10 bg-white pt-2">
-             <p className="text-gray-600 text-lg font-[500] -mt-1">eNaira</p> 
-             <div className="text-[8px] flex flex-col -mt-[8px] gap-[-4px] text-green-900">
-              <p>Same Naira</p>
-              <p className="-mt-1">More possibilities</p>
-             </div>
+              <p className="text-gray-600 text-lg font-[500] -mt-1">eNaira</p>
+              <div className="text-[8px] flex flex-col -mt-[8px] gap-[-4px] text-green-900">
+                <p>Same Naira</p>
+                <p className="-mt-1">More possibilities</p>
+              </div>
             </div>
-            <div  className="mx-[4px] mt-1">
-              <p className="text-[10px] font-semibold">Access your eNaria Wallet from UBA App</p>
-              <p className="text-[9px] mt-0.5">Secure,fast & easy way to deposit & withdraw & funds from / to your UBA app</p>
+            <div className="mx-[4px] mt-1">
+              <p className="text-[10px] font-semibold">
+                Access your eNaria Wallet from UBA App
+              </p>
+              <p className="text-[9px] mt-0.5">
+                Secure,fast & easy way to deposit & withdraw & funds from / to
+                your UBA app
+              </p>
             </div>
             <div className="flex justify-center mt-3">
-              <button className="bg-red-600 w-[90%] h-8 rounded-md">Get Started</button>
+              <button className="bg-red-600 w-[90%] h-8 rounded-md text-white text-sm">
+                Get Started              </button>
             </div>
+          </section>}
+
+          {showMobile && <section className="relative mx-auto pb-4 pl-2 pr-2 w-48 bg-white rounded-lg drop-shadow-xl mt-4  pt-3">
+            <h2 className=" text-sm font-bold mb-2">Mobile Top up</h2> 
+            <p className="text-[11px]">Customer Mobile Number</p>
+            <div className="mr-8 relative flex flex-col gap-4">
+              <input type="text"  placeholder="07031690110" className="rounded-[4px] outline-none border py-2 pl-[60px] pr- text-xs w-44"/>
+              <p className=" bg-red-600 w-[53px] py-[8.3px] px-1 text-[11px] text-white absolute top-0">NG +234</p>
+              <i className="fa-solid fa-xmark bg-gray-300 py-[4px] px-[6px] rounded-full text-white text-[10px] absolute top-2 right-[-26px] cursor-pointer"> </i>
+              <div className=" w-44 h-9 border text-xs flex items-center px-3 justify-between rounded-[4px]">
+                <p className="text-gray-400"> Select Account</p>
+                <i className="fa-solid fa-caret-down text-gray-600 cursor-pointer"></i>
+              </div>
+              <div className=" w-44 h-9 border text-xs flex items-center px-3 justify-between rounded-[4px]">
+                <p className="text-gray-400"> Select Service</p>
+                <i className="fa-solid fa-caret-down text-gray-600 cursor-pointer"></i>
+              </div>
+              <button className="bg-red-600 py-2 text-sm text-white  w-44 rounded-[4px]">Continue</button>
+              
+            </div>
+          </section>} 
+          <section className="flex  justify-between items-center gap-1 mx-auto pb-4 pl-2 pr-2 w-48 bg-white rounded-lg drop-shadow-xl mt-4  pt-4">
+            <p className=" text-sm">Customize Your Home</p>
+            <i className="fa-solid fa-plus bg-gray-300 py-2 px-2.5 text-red-600 text-sm rounded-md cursor-pointer"></i>
           </section>
             
 
-          <div className="fixed-footer absolute ">
-            <nav className="nav fixed w-[232px] h-10 bg-[#F1f1f1]  border-black rounded-[18px] bottom-[71px] text-red-600 text-xl flex justify-around items-center  gap-4 left-[26.5%] md:bottom-[72px] md:left-[41.5%]">
-              <i className="fa-solid fa-house cursor-pointer"></i>
-              <i className="fa-solid fa-bars cursor-pointer"></i>
+
+          <div className=" absolute ">
+            <nav className="nav fixed w-[232px] h-10 bg-[#F1f1f1] border-b-[4px] border-black rounded-[18px] bottom-[67px] text-red-600   left-[26.5%] md:bottom-[67px] md:left-[41.5%]">
+              <ul className="flex justify-around items-center gap-4 mt-1">
+                <li className="flex flex-col items-center">
+                  <i className="fa-solid fa-house cursor-pointer"></i>
+                  <p className="text-xs">Home</p>
+                </li>
+                <li className="flex flex-col items-center">
+                  <i className="fa-solid fa-bars cursor-pointer"></i>
+                  <p className="text-xs">More</p>
+                </li>
+              </ul>
             </nav>
           </div>
-
         </div>
       </main>
     </div>
